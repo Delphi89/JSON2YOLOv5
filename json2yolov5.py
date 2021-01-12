e6266dffa11238e29b2b985c614a3c4ddfe71bcd
Delphi89 <44812157+Delphi89@users.noreply.github.com>
2021-01-12 21:42:34 +0200

    Add files via upload

--- json2yolov5.py
+++ json2yolov5.py
@@ -1,5 +1,22 @@
 import os
 import json
+from PIL import Image
+
+#covert COCO to YOLOv5
+def convert(wi, hi, x1, y1, x2, y2):
+    dw = 1./wi
+    dh = 1./hi
+    x = (x1 + x2)/2.0 - 1
+    y = (y1 + y2)/2.0 - 1
+    w = x2 - x1
+    h = y2 - y1
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return (x,y,w,h)
+
+
 
 # create a folder for the YOLOv5 annotations if it doesn't exist
 if not os.path.exists('YOLOv5_results'):
@@ -8,26 +25,29 @@ if not os.path.exists('YOLOv5_results'):
 suffix = '.txt'
 
 # import the json file from the corect path
-with open('../../development/bdd100k/archive/bdd100k_labels_release/bdd100k/labels/bdd100k_labels_images_val.json') as f:
+with open('data/archive/bdd100k_labels_release/bdd100k/labels/bdd100k_labels_images_train.json') as f:
   data = json.load(f)
 
 print("start")
 
 # select the interesting data from the json file
 for p in data:
-    print(p['name'])
-     
+    filename1 = os.path.join('data/archive/bdd100k/bdd100k/images/100k/train',p['name'])
+    print("filename1: ", filename1)
+    im=Image.open(filename1)
+    wi,hi = im.size 
 
     #create the filename
-    filename = os.path.join('YOLOv5_results', p['name'][:-4] + suffix)
+    filename2 = os.path.join('YOLOv5_results/100k/train', p['name'][:-4] + suffix)
     # create a file with "filename" name
     
     for i in p["labels"]:
         print(i["category"])
         if (i["category"] != "drivable area") and (i["category"] != "lane") :     
-            mode = 'a' if os.path.exists(filename) else 'w'
-            with open(filename, mode) as f:
-                textline = str(i["category"]) + " " + str(i["box2d"]["x1"]) + " " + str(i["box2d"]["y1"]) + " " + str(i["box2d"]["x2"]) + " " + str(i["box2d"]["y2"])
+            x, y, w, h = convert(wi,hi,i["box2d"]["x1"], i["box2d"]["y1"], i["box2d"]["x2"], i["box2d"]["y2"])
+            mode = 'a' if os.path.exists(filename2) else 'w'
+            with open(filename2, mode) as f:
+                textline = str(i["category"]) + " " + str(x) + " " + str(y) + " " + str(w) + " " + str(h)
                 f.write(textline + '\n')
             print(i["box2d"]["x1"])  
             print(i["box2d"]["y1"]) 
@@ -35,9 +55,3 @@ for p in data:
             print(i["box2d"]["y2"])
     
     
-# save info to yolov5 format
-
-
-# create a file with "name" name
-# write each object name and coordinates to a line
-# save the file 
