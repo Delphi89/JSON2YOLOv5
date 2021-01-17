b0e93568cc41e5e7e795719415ffb44c72a6c7ac
Delphi89 <44812157+Delphi89@users.noreply.github.com>
2021-01-17 11:07:11 +0200

    Add files via upload

--- json2yolov5.py
+++ json2yolov5.py
@@ -2,6 +2,17 @@ import os
 import json
 from PIL import Image
 
+train_json_file = 'data/bdd100k/json/bdd100k_labels_images_train.json'
+val_json_file = 'data/bdd100k/json/bdd100k_labels_images_val.json'
+train_images_folder = 'data/bdd100k/images/train'
+val_images_folder = 'data/bdd100k/images/val'
+train_labels_folder = 'data/bdd100k/labels/train'
+val_labels_folder = 'data/bdd100k/labels/val'
+
+print(val_json_file)
+print(val_images_folder)
+print(val_labels_folder)
+
 #covert COCO to YOLOv5
 def convert(wi, hi, x1, y1, x2, y2):
     dw = 1./wi
@@ -19,59 +30,62 @@ def convert(wi, hi, x1, y1, x2, y2):
 #[ 'car', 'truck', 'person', 'traffic_sign', 'traffic_light', 'bus', 'rider', 'bike']
 def object_type_to_number(type):
     #print(type)
-    number = 0
+    number = 3344
     if type == "car":
-        number = 1
+        number = 0
     if type == "truck":
-        number = 2
+        number = 1
     if type == "person":
-        number = 3
+        number = 2
     if type == "traffic sign":
-        number = 4                
+        number = 3                
     if type == "traffic light":
-        number = 5
+        number = 4
     if type == "bus":
-        number = 6
+        number = 5
     if type == "rider":
-        number = 7
+        number = 6
     if type == "bike":
-        number = 8
+        number = 7
     #print(number)
     return number                            
 
 # create a folder for the YOLOv5 annotations if it doesn't exist
-if not os.path.exists('YOLOv5_results'):
-    os.makedirs('YOLOv5_results')
+if not os.path.exists('val_labels_folder'):
+    os.makedirs('val_labels_folder')
 
 suffix = '.txt'
 
 # import the json file from the corect path
-with open('data/archive/bdd100k_labels_release/bdd100k/labels/bdd100k_labels_images_train.json') as f:
+with open(val_json_file) as f:
   data = json.load(f)
 
-print("start")
+print("Start creating labels!")
 
 # select the interesting data from the json file
 for p in data:
-    filename1 = os.path.join('data/archive/bdd100k/bdd100k/images/100k/train',p['name'])
-    print("filename1: ", filename1)
+    filename1 = os.path.join(val_images_folder,p['name'])
+    #print("filename1: ", filename1)
     im=Image.open(filename1)
     wi,hi = im.size 
 
     #create the filename
-    filename2 = os.path.join('YOLOv5_results/100k/train', p['name'][:-4] + suffix)
+    filename2 = os.path.join(val_labels_folder, p['name'][:-4] + suffix)
+    #print(filename2)
     # create a file with "filename" name
     
     for i in p["labels"]:
         #print(i["category"])
-        if (i["category"] != "drivable area") and (i["category"] != "lane") :     
+        if (i["category"] != "drivable area") and (i["category"] != "lane") and (i["category"] != "motor") and (i["category"] != "train") :     
             x, y, w, h = convert(wi,hi,i["box2d"]["x1"], i["box2d"]["y1"], i["box2d"]["x2"], i["box2d"]["y2"])
             mode = 'a' if os.path.exists(filename2) else 'w'
             with open(filename2, mode) as f:
                 number = str(object_type_to_number(i["category"]))
-                #print(number)
+                if number == '3344':
+                    print("wrong category: ",i["category"])
+                    print("filename1: ", filename1)
                 textline = number + " " + str(x) + " " + str(y) + " " + str(w) + " " + str(h)
-                print(textline)
+                #print(textline)
                 f.write(textline + '\n')
             #print(i["box2d"]["x1"])  
             #print(i["box2d"]["y1"]) 
